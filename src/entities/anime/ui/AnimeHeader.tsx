80e795b0ea544ee16f35afb7d5338a4b05aa0e37
Catgobebra <elderlyjmih@gmail.com>
2026-01-11 23:40:03 +0500

    feat: safe optional chaining and fallbacks to AnimeHeader

--- src/entities/anime/ui/AnimeHeader.tsx
+++ src/entities/anime/ui/AnimeHeader.tsx
@@ -26,38 +26,46 @@ import {
 } from "@tabler/icons-react";
 
 import { useGetAnimeByIdQuery } from "../api/animeApi";
-import type { Anime,Genre } from '../model/types';
+import type {Genre } from '../model/types';
 
-export function AnimeHeader() {
-  const {data, isLoading } = useGetAnimeByIdQuery(43)
-  const anime = data?.data as Anime;
+interface AnimeHeaderProps {
+  animeId: number;
+}
 
-  if (isLoading) return <Container><Skeleton height={400} m={6} radius="xl" /></Container>
+export function AnimeHeader({animeId}: AnimeHeaderProps) {
+  const {data : animeResponse, isLoading: isLoadingAnime } = useGetAnimeByIdQuery(animeId)
+  const anime = animeResponse?.data;
+
+  const year = anime?.aired?.from 
+  ? new Date(anime.aired.from).getFullYear() 
+  : "—";
+
+  if (isLoadingAnime) return <Container><Skeleton height={400} m={6} radius="xl" /></Container>
   return (
     <Container>
       <BackgroundImage src="">
         <Flex gap={30}>
           <Image h={375} w={245} radius="lg"
-           src={anime.images.jpg.large_image_url} />
+           src={anime?.images.jpg.large_image_url || "https://placehold.co/180x110/333333/FFFFFF/png?text=No+Cover"} />
           <Stack justify="flex-end">
             <Text size="md" fw={500} mt="md">
-              {anime.title_english}
+              {anime?.title_english || "Error"}
             </Text>
             <Group align="center">
               <Group gap="xs" align="center">
                 <IconCalendarWeek size={16} />
-                <Text size="sm">{new Date(anime.aired.from).getFullYear()}</Text>
+                <Text size="sm">{year}</Text>
               </Group>
               <Group gap="xs" align="center">
                 <IconClockHour5 size={16} />
-                <Text size="sm">{anime.duration}</Text>
+                <Text size="sm">{anime?.duration || "-"}</Text>
               </Group>
               <Group>
                 <Badge tt="none" variant="default" color="gray">
-                  {anime.type}
+                  {anime?.type || "-"}
                 </Badge>
-                {anime.genres.map((genre : Genre,id : number) => {
-                  return (<Badge key={id} tt="none" variant="default" color="gray">
+                {anime?.genres.map((genre : Genre) => {
+                  return (<Badge key={genre.mal_id} tt="none" variant="default" color="gray">
                   {genre.name}
                 </Badge>)
                 })}
@@ -70,21 +78,21 @@ export function AnimeHeader() {
                   thickness={8}
                   label={
                     <Text size="xs" ta="center">
-                      <Text size="sm">{anime.score}</Text>
+                      <Text size="sm">{anime?.score}</Text>
                       <Space h={1} />
                       <Center>
                         <Rating size="xs" defaultValue={2} count={1} readOnly />
                       </Center>
                     </Text>
                   }
-                  sections={[{ value: anime.score*10, color: "#b78700" }]}
+                  sections={[{ value: (anime?.score || 0)*10 , color: "#b78700" }]}
                 />
                 <Stack gap={4}>
                   <Text size="md" fw={500}>
                     Rating
                   </Text>
                   <Text size="sm" c="dimmed">
-                    Scored by {anime.scored_by}
+                    Scored by {anime?.scored_by || 0}
                   </Text>
                 </Stack>
               </Group>
@@ -95,7 +103,7 @@ export function AnimeHeader() {
                   thickness={8}
                   label={
                     <Text size="xs" ta="center">
-                      <Text size="sm">{anime.rank}</Text>
+                      <Text size="sm">{anime?.rank || 0}</Text>
                     </Text>
                   }
                   sections={[]}
@@ -105,7 +113,7 @@ export function AnimeHeader() {
                     Place in the ranking
                   </Text>
                   <Text size="sm" c="dimmed">
-                    Favorited by {anime.favorites} users
+                    Favorited by {anime?.favorites || 0} users
                   </Text>
                 </Stack>
               </Group>
@@ -116,7 +124,7 @@ export function AnimeHeader() {
                 leftSection={<IconPlayerPlayFilled size={14} />}
                 color="#9a0006"
                 variant="filled"
-                href={anime.trailer.url}
+                href={anime?.trailer.url || ""}
               >
                 Watch Trailer
               </Button>
